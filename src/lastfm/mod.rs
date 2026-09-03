5e8e400ea422c79633444bb7c5ac19bb730ef876
asd <macintoshkrug@gmail.com>
2026-09-03 23:38:04 +0300

    scrobbling logic

--- src/lastfm/mod.rs
+++ src/lastfm/mod.rs
@@ -1,7 +1,21 @@
+use std::{
+    cmp,
+    time::{Duration, Instant},
+};
+
 static SERVICE: &str = "my-scrobbler";
+
+// seconds
+static MIN_DURATION: f64 = 30.0;
+static MAX_DURATION: f64 = 240.0;
 
 pub(crate) struct ScrobbleManager {
     track: Option<Track>,
+    has_scrobbled: bool,
+    is_playing: bool,
+    time_started: Instant,
+    last_play_ts: Instant,
+    accumulated_time: Duration,
 }
 
 #[derive(Debug)]
@@ -47,7 +61,14 @@ impl Track {
 
 impl ScrobbleManager {
     pub fn new() -> ScrobbleManager {
-        ScrobbleManager { track: None }
+        ScrobbleManager {
+            track: None,
+            has_scrobbled: false,
+            is_playing: false,
+            accumulated_time: Duration::default(),
+            time_started: Instant::now(),
+            last_play_ts: Instant::now(),
+        }
     }
 
     pub fn process_track(&mut self, track_data: Track, player_state: PlayerState) {
@@ -66,7 +87,26 @@ impl ScrobbleManager {
         };
     }
 
-    fn finalize_track(&self) {}
+    fn finalize_track(&mut self) {
+        if self.track.is_none() || self.has_scrobbled {
+            return;
+        }
+
+        self.add_accumulated_time();
+
+        if let Some(t) = &self.track {
+            if self.is_threshold_passed() {
+                println!("[i] SCROBBLED: {} - {}", t.artist, t.name);
+            } else {
+                let rounded =
+                    Duration::from_secs(self.accumulated_time.as_secs_f64().round() as u64);
+                println!("[i] Skipped: Listened for {:?}.", rounded)
+            }
+
+            self.track = None;
+            self.is_playing = false;
+        }
+    }
 
     fn is_same(&self, track_data: &Track) -> bool {
         match &self.track {
@@ -75,9 +115,63 @@ impl ScrobbleManager {
         }
     }
 
-    fn update_player_state(&self, player_state: PlayerState) {}
+    fn update_player_state(&mut self, player_state: PlayerState) {
+        match (player_state, self.is_playing) {
+            (PlayerState::Playing, false) => {
+                self.is_playing = true;
+                self.last_play_ts = Instant::now();
+                println!("[i] Resumed");
+            }
+            (PlayerState::Paused, true) => {
+                self.add_accumulated_time();
+                self.is_playing = false;
+                println!("[i] Paused. Accumulated time: {:?}", self.accumulated_time);
+            }
+            _ => {}
+        };
+    }
 
     fn start_new_track(&mut self, track_data: Track, player_state: PlayerState) {
         self.track = Some(track_data);
+        self.has_scrobbled = false;
+        self.accumulated_time = Duration::default();
+        self.time_started = Instant::now();
+
+        match player_state {
+            PlayerState::Playing => {
+                self.is_playing = true;
+                self.last_play_ts = Instant::now();
+
+                // api call
+
+                if let Some(t) = self.track.as_ref() {
+                    println!("[i] Now Playing: {} - {}", t.artist, t.name);
+                }
+            }
+            _ => {}
+        };
+    }
+
+    fn add_accumulated_time(&mut self) {
+        if self.is_playing {
+            self.accumulated_time += self.last_play_ts.elapsed();
+        }
+    }
+
+    fn is_threshold_passed(&self) -> bool {
+        self.track
+            .as_ref()
+            .and_then(|t| {
+                if t.duration <= MIN_DURATION {
+                    return Some(false);
+                }
+
+                // 50% of track length vs 4 minutes maximum threshold
+                let half_duration = t.duration / 2.0;
+                let threshold = Duration::from_secs_f64(half_duration.min(MAX_DURATION));
+
+                return Some(self.accumulated_time >= threshold);
+            })
+            .unwrap()
     }
 }
